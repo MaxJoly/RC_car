973fdb0ba25f26a166828c3d5dfee2fe8cde7b6a
Alexandre Bernard <alexandre.bernard@isen.fr>
2016-01-27 16:14:13 +0100

    getParam pour iDCmotor et iServomotor

--- src/DCmotor.cpp
+++ src/DCmotor.cpp
@@ -7,12 +7,13 @@
 #include "pwm.h"
 
 PWM pwmDCmotor(PWM_DC_MOTOR_DIR, 0, PWM_DC_MOTOR_PERIOD_NS, true, false);
-int DCmotor_period_speed_max = 0.0;
-int DCmotor_period_speed_0 = 0.0;
-int DCmotor_period_speed_min = 0.0;
-int DCmotor_speed_max = 0.0;
-int DCmotor_speed_min = 0.0;
+int DCmotor_period_speed_max = 0;
+int DCmotor_period_speed_0 = 0;
+int DCmotor_period_speed_min = 0;
+int DCmotor_speed_max = 0;
+int DCmotor_speed_min = 0;
 
+/*
 void refresh_param_DCmotor(const rc_car::CalibMsgConstPtr& calib)
 {
   ROS_INFO("iDCmotor received from tCalib:\nDCmotor_period_speed_max: %d\tDCmotor_period_speed_0: %d\tDCmotor_period_speed_min: %d\tDCmotor_speed_max: %d\tDCmotor_speed_min: %d\n", calib->DCmotor_period_speed_max, calib->DCmotor_period_speed_0, calib->DCmotor_period_speed_min, calib->DCmotor_speed_max, calib->DCmotor_speed_min);
@@ -23,6 +24,7 @@ void refresh_param_DCmotor(const rc_car::CalibMsgConstPtr& calib)
   DCmotor_speed_max = calib->DCmotor_speed_max;
   DCmotor_speed_min = calib->DCmotor_speed_min;
 }
+*/
 
 void refreshPWM_DCmotor(const rc_car::CommandConstPtr& cmd)
 {
@@ -30,7 +32,7 @@ void refreshPWM_DCmotor(const rc_car::CommandConstPtr& cmd)
 
   ROS_INFO("iDCmotor received from tCommand: %f", cmd->speed);
 
-  // Mise à jour du temps haut (en us) en fonction de speed
+  // Mise à jour du temps haut (en ns) en fonction de speed
   dutyPeriod = (DCmotor_period_speed_max - DCmotor_period_speed_min)/(DCmotor_speed_max - DCmotor_speed_min)*(cmd->speed) + DCmotor_period_speed_0;
   pwmDCmotor.setDuty(round(dutyPeriod));
 }
@@ -57,6 +59,7 @@ void RSR_process(const rc_car::RSRMsgConstPtr& RSR)
 int main(int argc, char **argv)
 {
   // TODO : vérifier que les commandes ont répondu et envoyer un message à tError sinon
+  std::string s;
   
   ros::init(argc, argv, "iDCmotor");
 
@@ -64,8 +67,58 @@ int main(int argc, char **argv)
 
   ros::Subscriber tRSR_sub = n.subscribe("tRSR", 1000, RSR_process);
   ros::Subscriber tCommand_sub = n.subscribe("tCommand", 1000, refreshPWM_DCmotor);
-  ros::Subscriber tCalib_sub = n.subscribe("tCalib", 1000, refresh_param_DCmotor);
+  //ros::Subscriber tCalib_sub = n.subscribe("tCalib", 1000, refresh_param_DCmotor);
   //ros::Publisher tError_pub = n.advertise<Error>("tError", 1000);
+
+  if (n.getParam("DCmotor_period_speed_max", s))
+  {
+    ROS_INFO("iDCmotor got param: %s\n", s.c_str());
+    DCmotor_period_speed_max = atoi(s.c_str());
+  }
+  else
+  {
+    ROS_ERROR("Failed to get param 'DCmotor_period_speed_max'\n");
+  }
+
+  if (n.getParam("DCmotor_period_speed_0", s))
+  {
+    ROS_INFO("iDCmotor got param: %s\n", s.c_str());
+    DCmotor_period_speed_0 = atoi(s.c_str());
+  }
+  else
+  {
+    ROS_ERROR("Failed to get param 'DCmotor_period_speed_0'\n");
+  }
+
+  if (n.getParam("DCmotor_period_speed_min", s))
+  {
+    ROS_INFO("iDCmotor got param: %s\n", s.c_str());
+    DCmotor_period_speed_min = atoi(s.c_str());
+  }
+  else
+  {
+    ROS_ERROR("Failed to get param 'DCmotor_period_speed_min'\n");
+  }
+
+  if (n.getParam("DCmotor_speed_max", s))
+  {
+    ROS_INFO("iDCmotor got param: %s\n", s.c_str());
+    DCmotor_speed_max = atoi(s.c_str());
+  }
+  else
+  {
+    ROS_ERROR("Failed to get param 'DCmotor_speed_max'\n");
+  }
+
+  if (n.getParam("DCmotor_speed_min", s))
+  {
+    ROS_INFO("iDCmotor got param: %s\n", s.c_str());
+    DCmotor_speed_min = atoi(s.c_str());
+  }
+  else
+  {
+    ROS_ERROR("Failed to get param 'DCmotor_speed_min'\n");
+  }
 
   ros::spin();
 
--- src/servomotor.cpp
+++ src/servomotor.cpp
@@ -13,6 +13,7 @@ int servo_period_angle_min = 0;
 int servo_angle_max = 0;
 int servo_angle_min = 0;
 
+/*
 void refresh_param_Servomotor(const rc_car::CalibMsgConstPtr& calib)
 {
   ROS_INFO("iServomotor received from tCalib:\nservo_period_angle_max: %d\tservo_period_angle_0: %d\tservo_period_angle_min: %d\tservo_angle_max: %d\tservo_angle_min: %d\n", calib->servo_period_angle_max, calib->servo_period_angle_0, calib->servo_period_angle_min, calib->servo_angle_max, calib->servo_angle_min);
@@ -23,6 +24,7 @@ void refresh_param_Servomotor(const rc_car::CalibMsgConstPtr& calib)
   servo_angle_max = calib->servo_angle_max;
   servo_angle_min = calib->servo_angle_min;
 }
+*/
 
 void refreshPWM_Servomotor(const rc_car::CommandConstPtr& cmd)
 {
@@ -30,7 +32,7 @@ void refreshPWM_Servomotor(const rc_car::CommandConstPtr& cmd)
 
   ROS_INFO("iServomotor received from tCommand: %f", cmd->dir);
 
-  // Mise à jour du temps haut (en us) en fonction de speed
+  // Mise à jour du temps haut (en ns) en fonction de speed
   dutyPeriod = (servo_period_angle_max - servo_period_angle_min)/(servo_angle_max - servo_angle_min)*(cmd->dir) + servo_period_angle_0;
   pwmServomotor.setDuty(round(dutyPeriod));
 }
@@ -57,15 +59,66 @@ void RSR_process(const rc_car::RSRMsgConstPtr& RSR)
 int main(int argc, char **argv)
 {
   // TODO : vérifier que les commandes ont répondu et envoyer un message à tError sinon
-  
+  std::string s;
+
   ros::init(argc, argv, "iServomotor");
 
   ros::NodeHandle n;
 
   ros::Subscriber tRSR_sub = n.subscribe("tRSR", 1000, RSR_process);
   ros::Subscriber tCommand_sub = n.subscribe("tCommand", 1000, refreshPWM_Servomotor);
-  ros::Subscriber tCalib_sub = n.subscribe("tCalib", 1000, refresh_param_Servomotor);
+  //ros::Subscriber tCalib_sub = n.subscribe("tCalib", 1000, refresh_param_Servomotor);
   //ros::Publisher tError_pub = n.advertise<Error>("tError", 1000);
+
+  if (n.getParam("servo_period_angle_max", s))
+  {
+    ROS_INFO("iServomotor got param: %s\n", s.c_str());
+    servo_period_angle_max = atoi(s.c_str());
+  }
+  else
+  {
+    ROS_ERROR("Failed to get param 'servo_period_angle_max'\n");
+  }
+
+  if (n.getParam("servo_period_angle_0", s))
+  {
+    ROS_INFO("iServomotor got param: %s\n", s.c_str());
+    servo_period_angle_0 = atoi(s.c_str());
+  }
+  else
+  {
+    ROS_ERROR("Failed to get param 'servo_period_angle_0'\n");
+  }
+
+  if (n.getParam("servo_period_angle_min", s))
+  {
+    ROS_INFO("iServomotor got param: %s\n", s.c_str());
+    servo_period_angle_min = atoi(s.c_str());
+  }
+  else
+  {
+    ROS_ERROR("Failed to get param 'servo_period_angle_min'\n");
+  }
+
+  if (n.getParam("servo_angle_max", s))
+  {
+    ROS_INFO("iServomotor got param: %s\n", s.c_str());
+    servo_angle_max = atoi(s.c_str());
+  }
+  else
+  {
+    ROS_ERROR("Failed to get param 'servo_angle_max'\n");
+  }
+
+  if (n.getParam("servo_angle_min", s))
+  {
+    ROS_INFO("iServomotor got param: %s\n", s.c_str());
+    servo_angle_min = atoi(s.c_str());
+  }
+  else
+  {
+    ROS_ERROR("Failed to get param 'servo_angle_min'\n");
+  }
 
   ros::spin();
 
